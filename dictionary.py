bff3d3b77606338bc5c70a218c186bf4b6eb0ee6
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-12-15 18:42:14 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+my_dict={'name':'Arnav', 'age':"11"}
+
+print(my_dict['name'])
+print(my_dict['age'])
+
+my_dict['age']='12'
+
+print(my_dict)
+
+my_dict['adress']='downtown'
+
+my_dict.pop('age')
+
+print(my_dict)
