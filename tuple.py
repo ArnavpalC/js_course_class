1f8a330e45e06a443b961b0b2c05f59eea0f273e
ArnavpalC <arnavpalchoudhury@gmail.com>
2025-12-16 18:25:51 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,14 @@
+my_tup = ('p', 'e', 'r', 'm', 'i', 't')
+
+print(my_tup[0])
+print(my_tup[5])
+
+n_tup = ('mouse',['8', '4', '1'], ('10', '9', '2'))
+
+print(n_tup[0][4])
+print(n_tup[1][1])
+print(n_tup[2][1:])
+
+for i in n_tup:
+    print(i)
+
