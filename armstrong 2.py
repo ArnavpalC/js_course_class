db5b52aa1ca9d2a2237c99293ac3e1e445ecab63
ArnavpalC <arnavpalchoudhury@gmail.com>
2026-02-23 18:27:00 +0530

    Add files via upload

---
+++ armstrong 2.py
@@ -0,0 +1,18 @@
+number = int(input('input you number: '))
+
+digits = len(str(number))
+
+resultNumber = 0
+
+temp = number
+while temp > 0:
+    digit = temp % 10
+    resultNumber += digit ** digits
+    temp //= 10
+
+
+if number == resultNumber:
+    print(number,'is an armstrong number')
+
+else:
+    print(f'{number} is not an armstrong number')
