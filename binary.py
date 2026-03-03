70684d6a0f026a172becc68002d0ffcbc1c69d02
ArnavpalC <arnavpalchoudhury@gmail.com>
2026-03-03 18:31:43 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,26 @@
+n1 = int(input('enter your first number: '))
+n2 = int(input('enter your second number: '))
+
+print('num1 and num2 = ',n1 & n2)
+
+
+print('\n num1  | num2 = ', n1 | n2)
+
+
+print('\n ~ num1= ', ~n1)
+
+print('\n ~ num2= ', ~n2)
+
+print('\n num1 ^ num2 = ', n1 ^ n2)
+
+
+print('\n num1 >> 1 = ', n1 >> 1)
+
+
+print('\n num2 >> 1 = ', n2 >> 1)
+
+
+print('\n num1 << 1 = ', n1 << 1)
+
+
+print('\n num2 << 1 = ', n2 << 1)
