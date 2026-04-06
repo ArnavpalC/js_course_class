52fd23395bad6b35b0fcdb7fddbf69d68e1fff32
ArnavpalC <arnavpalchoudhury@gmail.com>
2026-04-06 18:44:37 +0530

    Add files via upload

---
+++ x&y.py
@@ -0,0 +1,19 @@
+def cp(x, y):
+
+    result = 1
+
+    while(y > 0):
+
+        if(y % 2==0):
+            x = x * x
+            y >>=1
+
+        else:
+            result = result * x
+            y = y-1
+
+    return result
+
+x = int(input('enter x for x^y: '))
+y = int(input('enter y for x^y: '))
+print('total: ',cp(x,y))
